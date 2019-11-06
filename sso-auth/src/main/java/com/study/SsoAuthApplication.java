75cd1d22f21f92fd2bf2246d65d470cc37d7d388
汪福明 <523235748@qq.com>
2019-11-06 13:47:23 +0800

    初始化提交完成

--- sso-auth/src/main/java/com/study/SsoAuthApplication.java
+++ sso-auth/src/main/java/com/study/SsoAuthApplication.java
@@ -3,10 +3,8 @@ package com.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableDiscoveryClient
-@EnableHystrix
 @SpringBootApplication
 public class SsoAuthApplication {
 
--- sso-zuul/src/main/java/com/study/SsoZuulApplication.java
+++ sso-zuul/src/main/java/com/study/SsoZuulApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableDiscoveryClient
-@EnableZuulProxy
 @SpringBootApplication
+@EnableZuulProxy // 开启Zuul的网关功能
+@EnableDiscoveryClient //开启Eureka客户端发现功能
 public class SsoZuulApplication {
 
     public static void main(String[] args) {
